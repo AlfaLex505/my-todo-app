f28e28dd0d2945b433385a02ba9b727a83392e66
alexis <alexisalfaro505@gmail.com>
2025-07-23 16:25:33 -0600

    Correct the return of the UI in the hero

--- src/TODOHero.jsx
+++ src/TODOHero.jsx
@@ -2,13 +2,11 @@ const TODOHero = ( { todos_completed, total_todos } ) => {
     return(
         <section>
             <div>
-                <div>
-                    <p>Task Done</p>
-                    <p>Keep it up</p>
-                </div>
-                <div>
-                    {todos_completed}/{total_todos}
-                </div>
+                <p>Task Done</p>
+                <p>Keep it up</p>
+            </div>
+            <div>
+                {todos_completed}/{total_todos}
             </div>
         </section>
     );
