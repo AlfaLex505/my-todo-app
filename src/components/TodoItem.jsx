7a4c2a62a18aaa45de7eb760b950497cfa8eb8f1
alexis <alexisalfaro505@gmail.com>
2025-07-24 12:01:26 -0600

    Create the to-do component

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const TodoItem = ({ task, deleteTask, toggleCompleted }) => {
+
+    const handleChange = () => {
+        toggleCompleted(task.id);
+    }
+
+    return (
+        <div className="todo-item">
+            <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={handleChange}
+            />
+            <p>{task.text}</p>
+            <button onClick={() => deleteTask(task.id)}>
+                X
+            </button>
+        </div>
+    );
+}
+
+export default TodoItem;
