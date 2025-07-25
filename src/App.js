403551d74fec89fd7dd86fb96360beaa80277dde
alexis <alexisalfaro505@gmail.com>
2025-07-25 16:07:00 -0600

    Create a new header element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoList from "./components/TodoList"
+import TodoHeader from "./components/TodoHeader";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
+      <TodoHeader />
       <TodoList />
     </div>
   );
